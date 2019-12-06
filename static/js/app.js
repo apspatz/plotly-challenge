0ffbea22f4d0671e46682129111b2f023e1fedb8
unknown <andrew.p.spatz@gmail.com>
2019-12-05 19:16:45 -0500

    starting to work

--- static/js/app.js
+++ static/js/app.js
@@ -20,32 +20,81 @@ d3.json("samples.json").then((data)=>{
   console.log(data);
   var sample_id = data.names;
   var samples = data.samples;
-  init(samples[0]);
+  var metadata = data.metadata;
+  init(samples[0], metadata[0]);
+  dropBox(sample_id);
 });
 
-function init(sample){
+function init(sample, metadata){ //Building initial graphs and info
   console.log(sample);
-  
-  var sval = Object.values(sample.sample_values).slice(0,10);
-  var ids = Object.values(sample.otu_ids).slice(0,10);
-  var labels = Object.values(sample.otu_labels).slice(0,10);
-  console.log(sval, ids, labels);
+  // Bar Graph
+  var sval = Object.values(sample.sample_values).slice(0,10).reverse();
+  var ids = Object.values(sample.otu_ids).slice(0,10).reverse();
+  var labels = Object.values(sample.otu_labels).slice(0,10).reverse();
+  var otuids = ids.map(value => `OTU ${value}`)
 
-  var trace={
-    x: ids,
-    y: sval,
+  var bartrace={
+    x: sval,
+    y: otuids,
     text: labels,
     type: 'bar',
     orientation: 'h'
   }
-
-  var layout={
-    title: `Patient ${sample.id}`,
+  var barlayout={
+    title: `Subject ${sample.id}`,
     xaxis: {title: ''}
   }
+  var bardata = [bartrace];
+  Plotly.newPlot('bar', bardata, barlayout);
 
-  var data = [trace];
+  // Bubble Chart
+  var bubbletrace={
+    x: sample.otu_ids,
+    y: sample.sample_values,
+    mode: 'markers',
+    marker:{
+      size: sample.sample_values,
+      color: sample.otu_ids},
+    text: sample.otu_labels
+  }
+  var bubblelayout={
+    title: `Subject ${sample.id}`,
+    xaxis: {title: "OTU"}
+  }
+  var bubbledata = [bubbletrace];
+  Plotly.newPlot('bubble', bubbledata, bubblelayout);
 
-  Plotly.newPlot('bar', data, layout);
+  // Demographics
+  var demobox = d3.select('#sample-metadata');
+  demobox.html(`<table>
+                  <tr><th>id: ${metadata.id}</th></tr>
+                  <tr><th>ethnicity: ${metadata.ethnicity}</th></tr>
+                  <tr><th>gender: ${metadata.gender}</th></tr>
+                  <tr><th>age: ${metadata.age}</th></tr>
+                  <tr><th>location: ${metadata.location}</th></tr>
+                  <tr><th>bbtype: ${metadata.bbtype}</th></tr>
+                  <tr><th>wfreq: ${metadata.wfreq}</th></tr>
+                </table>`);
+};
+
+function dropBox(sample_ids){
+  console.log(sample_ids);
+  var dropdown = d3.select("#selDataset")
+                    .selectAll("option")
+                    .data(sample_ids)
+                    .enter()
+                    .append('option')
+                    .attr('value', function(id){
+                      return `${id}`;
+                    })
+                    .text(function(id){
+                      return `${id}`;
+                    });
+
+  
+  
+  
+  // for(id in sample_ids){
+  //   dropdown.html(`<option value ${id}=>${id}</option>`);
+  // }
 }
-
