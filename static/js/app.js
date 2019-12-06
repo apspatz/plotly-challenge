b5492a982a25b9272043137e933fae3a2388ec43
unknown <andrew.p.spatz@gmail.com>
2019-12-05 20:10:22 -0500

    Basic instructions complete

--- static/js/app.js
+++ static/js/app.js
@@ -1,32 +1,28 @@
-/**
- * Helper function to select stock data
- * Returns an array of values
- * @param {array} rows
- * @param {integer} index
- * index 0 - Date
- * index 1 - Open
- * index 2 - High
- * index 3 - Low
- * index 4 - Close
- * index 5 - Volume
- */
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
-
+// Retrieving sample data
 d3.json("samples.json").then((data)=>{
   console.log(data);
   var sample_id = data.names;
   var samples = data.samples;
   var metadata = data.metadata;
+  
+  // Builds initial plots and demographics
   init(samples[0], metadata[0]);
+
+  // Builds dropdown menu
   dropBox(sample_id);
+  
+  // Updates plots and demographics upon new selection
+  var dropdown = d3.select("#selDataset");
+  dropdown.on("change", function(){
+    var new_id = dropdown.property('value');
+    console.log(new_id);
+    var new_index = sample_id.indexOf(new_id);
+    console.log(new_index);
+    updatePlotly(samples[new_index], metadata[new_index]);
+  });
 });
 
 function init(sample, metadata){ //Building initial graphs and info
-  console.log(sample);
   // Bar Graph
   var sval = Object.values(sample.sample_values).slice(0,10).reverse();
   var ids = Object.values(sample.otu_ids).slice(0,10).reverse();
@@ -77,8 +73,8 @@ function init(sample, metadata){ //Building initial graphs and info
                 </table>`);
 };
 
+// Builds dropdown menu with all IDs
 function dropBox(sample_ids){
-  console.log(sample_ids);
   var dropdown = d3.select("#selDataset")
                     .selectAll("option")
                     .data(sample_ids)
@@ -90,11 +86,59 @@ function dropBox(sample_ids){
                     .text(function(id){
                       return `${id}`;
                     });
+}
 
+// Updates plots and demographics based on new ID
+function updatePlotly(sample, metadata){
+  // Bar Chart updates
+  var sval = Object.values(sample.sample_values).slice(0,10).reverse();
+  var ids = Object.values(sample.otu_ids).slice(0,10).reverse();
+  var labels = Object.values(sample.otu_labels).slice(0,10).reverse();
+  var otuids = ids.map(value => `OTU ${value}`)
+
+  var new_x = sval;
+  var new_y = otuids;
+  var new_text = labels;
+  var new_title = `Subject ${sample.id}`;
+  
+  Plotly.restyle("bar", "x", [new_x]);
+  Plotly.restyle("bar", "y", [new_y]);
+  Plotly.restyle("bar", "text", new_text);
+  Plotly.relayout("bar", 'title', new_title);
   
   
-  
-  // for(id in sample_ids){
-  //   dropdown.html(`<option value ${id}=>${id}</option>`);
-  // }
+
+  // Bubble Chart updates
+  var bubbletrace={
+    x: sample.otu_ids,
+    y: sample.sample_values,
+    mode: 'markers',
+    marker:{
+      size: sample.sample_values,
+      color: sample.otu_ids},
+    text: sample.otu_labels
+  }
+  var new_x = sample.otu_ids;
+  var new_y = sample.sample_values;
+  var new_text = sample.otu_labels;
+
+  Plotly.restyle("bubble", "x", [new_x]);
+  Plotly.restyle("bubble", "y", [new_y]);
+  Plotly.restyle("bubble", "marker.size", [new_y]);
+  Plotly.restyle("bubble", "marker.color", [new_x]);
+  Plotly.restyle("bubble", "text", [new_text]);
+  Plotly.relayout("bubble", 'title', new_title);
+
+  // Demographics updates
+  var demobox = d3.select('#sample-metadata');
+  demobox.html('');
+  demobox.html(`<table>
+                  <tr><th>id: ${metadata.id}</th></tr>
+                  <tr><th>ethnicity: ${metadata.ethnicity}</th></tr>
+                  <tr><th>gender: ${metadata.gender}</th></tr>
+                  <tr><th>age: ${metadata.age}</th></tr>
+                  <tr><th>location: ${metadata.location}</th></tr>
+                  <tr><th>bbtype: ${metadata.bbtype}</th></tr>
+                  <tr><th>wfreq: ${metadata.wfreq}</th></tr>
+                </table>`);
 }
